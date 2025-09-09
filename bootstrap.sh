5eb93db790f9d43bd3fa17dac7b51d0f39434262
Erik From <erik_from@hotmail.com>
2025-09-09 23:45:25 +0200

    added OLD_NEEDED to not always create extra directory

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,18 +6,28 @@ DATE=$(date +%Y%m%d%H%M%S)
 olddir=~/.dotfiles_old/$DATE
 # list of files/folders to symlink in homedir
 files=".aliases.d .bash_aliases .gitconfig .gitconfig.d .vimrc .vim"
+OLD_NEEDED=0
+for file in $files; do
+	OLD_NEEDED=$(expr $OLD_NEEDED + $(ls ~/$file 2>/dev/null | wc -l))
+done
 
 ##########
-
-mkdir -vp $olddir
+if [ "$OLD_NEEDED" -gt 0 ]; then
+	mkdir -vp $olddir
+fi
 
 echo "Changing to the $dir directory"
 cd $dir
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
-echo "Moving any existing dotfiles from ~ to $olddir and creating symlinks in home directory."
+if [ "$OLD_NEEDED" -gt 0 ]; then
+	echo "Moving any existing dotfiles from ~ to $olddir and"
+fi
+echo "Creating symlinks in home directory."
 for file in $files; do
-	mv -v ~/$file $olddir/$file 2>/dev/null
+	if [ "$OLD_NEEDED" -gt 0 ]; then
+		mv -v ~/$file $olddir/$file 2>/dev/null
+	fi
 	ln -vs $dir/$file ~/$file
 done
 
